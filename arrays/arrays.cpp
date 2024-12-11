7cdc7656477a5bf86bd1a5718cef78fa41ab21b9
joao vitor <joaovitoroliveirasoarespereira@gmail.com>
2024-12-10 22:04:00 -0300

    ###

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <array>
 
+//Armazena uma sequência de comprimento fixo de elementos.
+
 int main()
 {
 	std::array<std::string, 5> names{"Joao", "Vitor", "Oliveira", "Soares", "Pereira"};
